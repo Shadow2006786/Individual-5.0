d77e367b77c958a888112a3139645ca3cf97d71e
Borzua <36938572+Shadow2006786@users.noreply.github.com>
2024-12-13 01:18:05 +0200

    Add files via upload

---
+++ src/main/java/ua/edu/chmnu/net_dev/c4/mail/CheckingMails.java
@@ -0,0 +1,67 @@
+package ua.edu.chmnu.net_dev.c4.mail;
+
+import java.util.Properties;
+import javax.mail.*;
+
+public class CheckingMails {
+
+    public static void check(String host, String storeType, String user, String password) {
+        Store store = null;
+        Folder emailFolder = null;
+
+        try {
+            Properties properties = new Properties();
+            properties.put("mail.pop3.host", host);
+            properties.put("mail.pop3.port", "995");
+            properties.put("mail.pop3.starttls.enable", "true");
+
+            Session emailSession = Session.getDefaultInstance(properties);
+
+            store = emailSession.getStore("pop3s");
+            store.connect(host, user, password);
+
+            emailFolder = store.getFolder("INBOX");
+            emailFolder.open(Folder.READ_ONLY);
+
+            Message[] messages = emailFolder.getMessages();
+            System.out.println("messages.length---" + messages.length);
+
+            for (int i = 0, n = messages.length; i < n; i++) {
+                Message message = messages[i];
+                System.out.println("---------------------------------");
+                System.out.println("Email Number " + (i + 1));
+                System.out.println("Subject: " + message.getSubject());
+                if (message.getFrom() != null && message.getFrom().length > 0) {
+                    System.out.println("From: " + message.getFrom()[0]);
+                }
+            }
+
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (emailFolder != null && emailFolder.isOpen()) {
+                    emailFolder.close(false);
+                }
+                if (store != null) {
+                    store.close();
+                }
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String host = "pop.gmail.com";
+        String mailStoreType = "pop3";
+        String username = "shadow912999@gmail.com";
+        String password = "password";
+
+        check(host, mailStoreType, username, password);
+    }
+}
